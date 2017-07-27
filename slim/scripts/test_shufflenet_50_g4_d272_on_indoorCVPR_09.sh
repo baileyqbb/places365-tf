c92c8168175b0a762b50eb1316dcef6e6469a5ae
baileyqbb <baileyqbb@gmail.com>
2017-07-27 15:54:13 +0800

    Add shufflenet net finetune and test scripts

---
+++ slim/scripts/test_shufflenet_50_g4_d272_on_indoorCVPR_09.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# This script performs the following operations:
+# 
+# 2. Test the trained shufflenet_50_g4_d272 model on the indoorCVPR_09 training set.
+# 3. Evaluates the model on the places365 validation set.
+#
+# Usage:
+# cd slim
+# ./slim/scripts/test_shufflenet_50_g4_d272_on_indoorCVPR_09.sh
+set -e
+
+IMAGE_FILE=/home/qianbb/PycharmProjects/places365-tf/First_Student_IC_school_bus_202076.jpg
+
+CHECKPOINT_PATH=/home/qianbb/PycharmProjects/places365-tf/models-indoorCVPR_09/mobilenet_v1_1_224/all/
+#CHECKPOINT_PATH=/home/qianbb/Projects/places365-tf/models/pre-trained/inception_resnet_v2_2016_08_30.ckpt
+
+python predict_image_classifier.py \
+  --image_file=${IMAGE_FILE} \
+  --num_classes=67 \
+  --model_name=shufflenet_50_g4_d272 \
+  --preprocessing_name=shufflenet_50_g4_d272 \
+  --checkpoint_path=${CHECKPOINT_PATH}
